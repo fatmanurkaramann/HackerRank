0d073f777b0bc96e40bd85d2da197ab62c46642f
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-01-10 21:08:53 +0300

    1 hacerrank problemi çözüldü

--- src/Problems/hacker-rank-solutions.ts
+++ src/Problems/hacker-rank-solutions.ts
@@ -65,8 +65,93 @@ export class HackerRankSolutions {
       sum += sortedArr[i];
     }
     console.log(sum - sortedArr[0]);
-    console.log(sum - (sortedArr[sortedArr.length-1]));
+    console.log(sum - sortedArr[sortedArr.length - 1]);
+  }
+  public timeConversion(s: any) {
+    var time = s.toLowerCase().split(":");
+    var hours = parseInt(time[0]);
+    var _ampm = time[2];
+    if (_ampm.indexOf("am") != -1 && hours == 12) {
+      time[0] = "00";
+    }
+    if (_ampm.indexOf("pm") != -1 && hours < 12) {
+      time[0] = hours + 12;
+    }
+    return time.join(":").replace(/(am|pm)/, "");
+  }
+  public grade(grades: []) {
+    const result: number[] = [];
+    for (const n of grades) {
+      const nextMultiple = n + (5 - (n % 5));
+      if (nextMultiple - n < 3 && n > 37) {
+        result.push(nextMultiple);
+      } else if (nextMultiple - n >= 3) {
+        result.push(n);
+      } else if (n < 37) {
+        result.push(n);
+      }
+    }
+    return result;
+  }
+
+  public kangaroo(x1: number, v1: number, x2: number, v2: number): string {
+    let t: number = (x2 - x1) / (v1 - v2);
+    if (
+      v1 * t + x1 == v2 * t + x2 &&
+      t > 0 &&
+      typeof t === "number" &&
+      Number.isInteger(t)
+    ) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
+  public breakingRecords(scores: number[]): number[] {
+    let fullScore: number[] = [];
+    const HighestScore: number[] = [];
+    const LowestScore: number[] = [];
+    let HighScore = scores[0];
+    let lowScore = scores[0];
+    for (let i = 0; i < scores.length; i++) {
+      if (scores[i] > HighScore) {
+        HighScore = scores[i];
+
+        if (scores[i] > scores[i - 1]) {
+          HighestScore.push(scores[i]);
+        } else if (HighScore > scores[i]) {
+          HighestScore.push(HighScore);
+        }
+      } else if (scores[i] < lowScore) {
+        lowScore = scores[i];
+        if (scores[i] < scores[i - 1] && lowScore) {
+          LowestScore.push(scores[i]);
+        }
+      }
+    }
+    let highLength = HighestScore.length;
+    fullScore.push(highLength);
+    let lowLength = LowestScore.length;
+    fullScore.push(lowLength);
+    return fullScore;
+  }
+  public birthdayCakeCandles(candles: number[]): number {
+    const largestNumber = Math.max(...candles);
+    const occurrences = candles.filter((number) => number === largestNumber);
+    return occurrences.length;
+  }
+  public sockMerchant(n: number, ar: number[]): number {
+    const result:{id:number,count:number}[] = []
+    let numberOfPairs = 0
+    for (let i = 0; i < ar.length; i++) {
+      let current = result.find((x)=>x.id == ar[i])
+      current == null
+      ? result.push({ id: ar[i], count: 1 })
+      : current.count++;
+      if(current!=null && current.count % 2 == 0){
+        numberOfPairs++
+      }
+    }
+    return numberOfPairs
   }
 }
-
-
